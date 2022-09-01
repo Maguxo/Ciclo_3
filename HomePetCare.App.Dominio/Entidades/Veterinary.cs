7a5a7a9348e0eda6819b5e1b07e2563b196a916c
magucho <ednemesis-006hotmail.com>
2022-08-31 21:45:35 -0500

    Se agrega proyecto

---
+++ HomePetCare.App.Dominio/Entidades/Veterinary.cs
@@ -0,0 +1,14 @@
+
+//clase veterinario
+namespace HomePetCare.App.Dominio
+{
+    public class Veterinary
+    { 
+        public int Id{get;set;}
+        public string NameVet{get;set;}
+        public string LastNameVet {get;set;}
+        public int  PhoneVet{get;set;}
+        public string ProfCard{get;set;}
+        
+    }
+}
