4d5a9a7284bc563901902f2541b9f94d3a1884e7
magucho <ednemesis-006hotmail.com>
2022-09-07 08:52:42 -0500

    imagenes agregado

--- HelloWeb.App/HelloWeb.App.Frontend/Pages/informa/Details.cshtml.cs
+++ HelloWeb.App/HelloWeb.App.Frontend/Pages/informa/Details.cshtml.cs
@@ -1,25 +1,26 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using HelloWeb.App.Dominio;
 
 namespace HelloWeb.App.Frontend.Pages
 {
     public class DetailsModel : PageModel
     {
-        private readonly IRepositorioConsulta irepositorioCobsulta;
+        //private readonly IRepositorioConsulta irepositorioCobsulta;
 
-        public DetailsModel(IRepositorioConsulta irepositorioConsulta)
-        {
-            this.irepositorioCobsulta= irepositorioConsulta;
-        }
-        public IActionResult OnGet(int infoId)
-        {
-             Saludo= irepositorioCobsulta.GetInformaPorId(infoId);
-             if(Saludo ==null)
-             {
-                return RedirectToPage("./NotFound");
-             }
-             else
-             return Page();
-        }
+        //public DetailsModel(IRepositorioConsulta irepositorioConsulta)
+        //{
+          //  this.irepositorioCobsulta= irepositorioConsulta;
+        //}
+        //public IActionResult OnGet(int infoId)
+        //{
+             //Saludo= irepositorioCobsulta.GetInformaPorId(infoId);
+             //if(Saludo ==null)
+             //{
+               // return RedirectToPage("./NotFound");
+             //}
+             //else
+             //return Page();
+        //}
     }
 }
